0823942d8bf9b8490cb562831aa3f0bc5b5a6009
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-09-15 11:23:27 +0300

    Add initial vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,22 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = ""
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Env = var.environment
+    Terraform = var.terraform
+  }
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,5 +1,15 @@
 variable "environment" {
   description = "The environment in which the resource belongs"
   type = string
-  default = "dev"
+}
+
+variable "terraform" {
+  description = "Confirms that the following piece of infrastructure was created using Terraform"
+  type = bool
+  default = true
+}
+
+variable "cidr_block" {
+  description = "The CIDR block desired on the VPC"
+  type = string
 }
