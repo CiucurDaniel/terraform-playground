7ab4090cad8a48dc546fefc74334894f0b4d8a2b
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-09-15 14:02:16 +0300

    Update subnet module

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -19,14 +19,14 @@ resource "aws_subnet" "subnet" {
   availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "${var.name}${count.index}"
+    Name = "Subnet for ${var.environment} no ${count.index}"
   }
 }
 
 resource "aws_route_table" "table" {
   vpc_id = var.vpc_id
   tags = {
-    Name = var.name
+    Name = var.rt_name
     Env = var.environment
     Terraform = var.terraform
   }
