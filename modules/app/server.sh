7adbcc842630df337febc04e32de0c7a57b7b0a0
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-09-20 17:06:00 +0300

    Remove redundant provider

--- modules/app/server.sh
+++ modules/app/server.sh
@@ -6,7 +6,18 @@ yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
 
+# check terraform behaviour
+port = 7
+echo "this is confusing: $port vs ${port}"
+
+
+# use terraform vars in ALL-CAPS
+
+# TODO: Update me
+
 echo "<h1>Hello terraform-playground </h1>" >index.html
 echo "<p> The current environment is: ${environment} </p>" >>index.html
 echo "<p> My port is ${port} </p>" >>index.html
 nohup busybox httpd -f -p "${port}" &
+
+# python -m SimpleHTTPServer ${port}
